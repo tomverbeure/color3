2e99fef4b94a138b1b54bc7bfb50a1c4845252a9
Tom Verbeure <hombre+github@gmail.com>
2018-04-11 05:52:17 +0000

    All main connection from SiI9233 to FPGA mapped

--- reveng/rtl/top.v
+++ reveng/rtl/top.v
@@ -2,13 +2,15 @@
 module top(
 	input 		osc25_pad_in,
 
+	// MISC
 	output		led_r_pad_out,
 	output		led_g_pad_out,
 	output		led_b_pad_out,
 
-	input 		ir_rx,
-	input 		button,
+	input 		ir_rx /* synthesis keep */,
+	input 		button /* synthesis keep */,
 
+	// SDRAM
 	input 		dram_clk_pad_out,
 	input 		dram_cs_n_pad_out,
 	input 		dram_we_n_pad_out,
@@ -20,70 +22,34 @@ module top(
 	input [15:0]	dram_dq_pad_inout,
 	input [1:0]	dram_dqm_pad_inout,
 
-/*
-	input 		sii9233_clk,
-	input 		sii9233_vsync,
-	input 		sii9233_hsync,
-	input 		sii9233_de,
-	input [35:0]	sii9233_q,
-	input 		sii9233_evnodd,
+	// SII9233
+	output		sii9233_reset_,
+	input		sii9233_int		/* synthesis keep */,
+	inout		sii9233_cscl,
+	inout		sii9233_csda,
 
-	input 		sii9233_int,
-	input 		sii9233_reset_,
-	input 		sii9233_cscl,
-	input 		sii9233_csda,
-	input 		sii9233_ci2ca,
-	input 		sii9233_scdt
-*/
+	input 		sii9233_de		/* synthesis keep */,
+	input 		sii9233_hsync		/* synthesis keep */,
+	input 		sii9233_vsync		/* synthesis keep */,
+	input 		sii9233_idck		/* synthesis keep */,
 
-	input 		sii9136_reset_,
-	input 		sii9136_int,
-	input 		sii9136_cscl,
-	input 		sii9136_csda,
+	input [35:0]	sii9233_q		/* synthesis keep */,
 
-	input 		sii9136_de,
-	input 		sii9136_hsync,
-	input 		sii9136_vsync,
-	input 		sii9136_idck,
 
-	input 		sii9136_d0,
-	input 		sii9136_d1,
-	input 		sii9136_d2,
-	input 		sii9136_d3,
-	input 		sii9136_d4,
-	input 		sii9136_d5,
-	input 		sii9136_d6,
-	input 		sii9136_d7,
-	input 		sii9136_d8,
-	input 		sii9136_d9,
-	input 		sii9136_d10,
-	input 		sii9136_d11,
-	input 		sii9136_d12,
-	input 		sii9136_d13,
-	input 		sii9136_d14,
-	input 		sii9136_d15,
-	input 		sii9136_d16,
-	input 		sii9136_d17,
-	input 		sii9136_d18,
-	input 		sii9136_d19,
-	input 		sii9136_d20,
-	input 		sii9136_d21,
-	input 		sii9136_d22,
-	input 		sii9136_d23,
-	input 		sii9136_d24,
-	input 		sii9136_d25,
-	input 		sii9136_d26,
-	input 		sii9136_d27,
-	input 		sii9136_d28,
-	input 		sii9136_d29,
-	input 		sii9136_d30,
-	input 		sii9136_d31,
-	input 		sii9136_d32,
-	input 		sii9136_d33,
-	input 		sii9136_d34,
-	input 		sii9136_d35,
+	// SII9136
+	output 		sii9136_reset_,
+	input 		sii9136_int		/* synthesis keep */,
+	inout 		sii9136_cscl,
+	inout 		sii9136_csda,
 
-	input [200:0]	misc_input /* synthesis keep */
+	output 		sii9136_de,
+	output 		sii9136_hsync,
+	output 		sii9136_vsync,
+	output 		sii9136_idck,
+
+	output [35:0]	sii9136_d,
+
+	input [149:0]	misc_input		/* synthesis keep */
 	);
 
 
@@ -95,5 +61,20 @@ module top(
 	assign led_r_pad_out = cntr[25];
 	assign led_g_pad_out = cntr[24];
 	assign led_b_pad_out = cntr[23];
+
+	assign	sii9136_reset_ = 1'b0;
+	assign 	sii9136_cscl = 1'bz;
+	assign 	sii9136_csda = 1'bz;
+
+	assign 	sii9136_de = 1'b0;
+	assign 	sii9136_hsync = 1'b0;
+	assign 	sii9136_vsync = 1'b0;
+	assign 	sii9136_idck = 1'b0;
+
+	assign sii9136_d = 36'd0;
+
+	assign	sii9233_reset_ = 1'b0;
+	assign 	sii9233_cscl = 1'bz;
+	assign 	sii9233_csda = 1'bz;
 
 endmodule
