a70d82ec2d209235243730ed6d3c64270e6d6410
Tom Verbeure <hombre+github@gmail.com>
2018-04-21 21:58:21 +0000

    More register definitions

--- reveng/recordings/process.rb
+++ reveng/recordings/process.rb
@@ -4,9 +4,10 @@ require 'pp'
 
 SII9233Regs = {
     0x60 => {
-        0x00 => [ "Chip Present" ],
-        0x02 => [ "Chip ID (l)" ],
-        0x03 => [ "Chip ID (h)" ],
+        0x00 => [ "Chip Vendor ID (l)" ],
+        0x01 => [ "Chip Vendor ID (h)" ],
+        0x02 => [ "Chip Device ID (l)" ],
+        0x03 => [ "Chip Device ID (h)" ],
         0x04 => [ "Chip Rev" ],
         0x05 => [ "SW Reset" ],
         0x06 => [ "System Status" ],
@@ -51,6 +52,7 @@ SII9233Regs = {
         0x74 => [ "Interrupt Status 4" ],
         0x75 => [ "Interrupt Unmask 1" ],
         0x77 => [ "Interrupt Unmask 3" ],
+        0x79 => [ "Interrupt Control" ],
         0x7a => [ "Interrupt Info Frame Control" ],
         0x7b => [ "Interrupt Status 5" ],
         0x7c => [ "Interrupt Status 6" ],
@@ -66,12 +68,66 @@ SII9233Regs = {
         0x92 => [ "Interrupt Unmask 7" ],
         0x93 => [ "Interrupt Unmask 8" ],
         0x94 => [ "INFM_CLR" ],
-        0xB4 => [ "Auto Audio Unmute Control" ],
-        0xB5 => [ "Auto Exception Control" ],
-        0xB6 => [ "AEC Exception Enable 1" ],
-        0xBA => [ "AEC Exception Enable 2" ],
-        0xBB => [ "AEC Exception Enable 3" ],
-    }
+        0xb4 => [ "Auto Audio Unmute Control" ],
+        0xb5 => [ "Auto Exception Control" ],
+        0xb6 => [ "AEC Exception Enable 1" ],
+        0xba => [ "AEC Exception Enable 2" ],
+        0xbb => [ "AEC Exception Enable 3" ],
+    },
+    0x68 => {
+        0x26 => [ "RX_I2S_CTRL1" ],
+        0x27 => [ "RX_I2S_CTRL2" ],
+        0x29 => [ "RX_AUDIO_CTRL" ],
+        0x2e => [ "RX_AUDIO_SWAP" ],
+        0x34 => [ "RX_AUDP_STAT" ],
+        0x37 => [ "RX_AUDP_MUTE" ],
+    },
+    0xee => {
+        0x01 => [ "EN_EDID" ],
+        0x02 => [ "EDID_FIFO_ADDR" ],
+        0x03 => [ "EDID_FIFO_DATA" ],
+        0x04 => [ "EDID_FIFO_SEL" ],
+        0x05 => [ "NVM_COMMAND" ],
+        0x07 => [ "NVM_COMMAND_DONE" ],
+        0x08 => [ "BSM_INIT" ],
+        0x09 => [ "BSM_STAT" ],
+        0x10 => [ "NVM_STAT" ],
+        0x13 => [ "HDP_HW_CTRL" ],
+        0x1a => [ "CECPA_ADDR" ],
+        0x40 => [ "RPI_AUTO_CONFIG" ],
+        0x41 => [ "WAIT_CYCLE" ],
+        0xd0 => [ "CLKDETECT_STATUS" ],
+        0xd1 => [ "PWR5V_STATUS" ],
+        0xd4 => [ "CBUS_PAD_SC" ],
+        0xe9 => [ "DRIVE_CNTL" ],
+    },
+    0x00 => {
+        0x3f => [ "* PD_SYS" ],
+        0x3e => [ "* PD_SYS2" ],
+        0x39 => [ "* RX_FIFO_DIFF" ],
+        0x0a => [ "* RX_AFE_DDR_CONF" ],
+        0x0b => [ "* RX_AFE_DSA_CONF" ],
+        0x1f => [ "* RX_RI_PRIME" ],
+        0x31 => [ "* RX_HDCP_CTRL, RX_CH_STATUS5" ],
+        0x32 => [ "* RX_HDCP_STAT" ],
+        0x21 => [ "* RX_AKSV" ],
+        0x1a => [ "* RX_BKSV" ],
+        0xbb => [ "* RX_ECC_CTRL" ],
+        0x7c => [ "* RX_ECC_THRES" ],
+        0xcd => [ "* RX_HDCP_ERR" ],
+        0x8c => [ "* RX_PLL_IOPPC" ],
+        0x8d => [ "* RX_PLL_CGC" ],
+        0x00 => [ "* RX_ACR_CTRL1" ],
+        0x02 => [ "* RX_FREQ_SVAL" ],
+        0x06 => [ "* RX_HW_N_ADDR" ],
+        0x0c => [ "* RX_HW_CTS" ],
+        0x28 => [ "* RX_I2S_MAP" ],
+        0x2a => [ "* RX_CH_STATUS1" ],
+        0x30 => [ "* RX_CH_STATUS4" ],
+        0x35 => [ "* RX_HDMI_CRIT1" ],
+        0x2d => [ "* RX_MUTE_DIV" ],
+        0x38 => [ "* RX_HDMI_CRIT2" ],
+    },
 }
 
 class I2CData
@@ -110,7 +166,10 @@ class I2CPacket
 
             reg_info = (SII9233Regs[ addr ][ self.reg_nr ] ) rescue nil
             if reg_info == nil
-                reg_info = [ "<Unknown>" ]
+		reg_info = (SII9233Regs[ 0x00 ][ self.reg_nr ] ) rescue nil
+		if reg_info == nil
+              	    reg_info = [ "<Unknown>" ]
+		end
             end
 
             if reg_info && self.wr
